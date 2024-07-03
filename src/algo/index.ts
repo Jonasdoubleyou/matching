decb939ca8093f7478c19a65e2e60410060a7130
Jonas Wilms <jonas@wilms.ninja>
2024-07-03 21:49:33 +0200

    feat: Add Edmonds Matcher

--- src/algo/index.ts
+++ src/algo/index.ts
@@ -1,6 +1,7 @@
 import { GreedyMatcher } from "./greedy";
 import { PathGrowingMatcher } from "./path_growing";
 import { NaiveMatcher } from "./naive";
+import { EdmondsMatcher as EdmondsMatcher_Broken } from "./edmonds";
 
 export * from "./base";
 export { GreedyMatcher } from "./greedy";
@@ -9,7 +10,8 @@ export { GreedyMatcher } from "./greedy";
 export const matchers = {
     GreedyMatcher,
     PathGrowingMatcher,
-    NaiveMatcher
+    NaiveMatcher,
+    EdmondsMatcher_Broken
 } as const;
 
 export type MatcherName = keyof typeof matchers;
